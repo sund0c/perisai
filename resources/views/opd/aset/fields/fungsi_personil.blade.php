c2bcd2627a0b130d89fa25acc60966c97cddab64
sund0c <sundiakdoctoral@gmail.com>
2025-10-02 15:24:10 +0800

    department

--- resources/views/opd/aset/fields/fungsi_personil.blade.php
+++ resources/views/opd/aset/fields/fungsi_personil.blade.php
@@ -1,4 +1,4 @@
 <div class="form-group">
-    <label for="fungsi_personil">Departmemen/Bagian/Bidang Personil</label>
+    <label for="fungsi_personil">Departemen/Bagian/Bidang Personil</label>
     <input type="text" name="fungsi_personil" id="fungsi_personil" class="form-control" value="{{ old('fungsi_personil', $aset->fungsi_personil ?? '') }}">
 </div>
